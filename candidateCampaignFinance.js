c1f1e5c4866014fbdd237bdc8c57929d68c15c8e
Sujay Khandekar <sskhandek@gmail.com>
2015-03-05 19:00:13 -0600

    Bernard is awesome

--- candidateCampaignFinance.js
+++ candidateCampaignFinance.js
@@ -1,4 +1,4 @@
-//SUJAY IS THE BEST
+//SUJAY & BERNARD ARE THE BEST
 
 var key = "api-key=c15b2a78829ba58bbdd915dec1fcbbad%3A13%3A71411641"
 var address = "http://api.nytimes.com/svc/elections/us/v3/finances/2008-present/candidates/P80003338.xml?"
